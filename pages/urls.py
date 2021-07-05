1af0350fc2ef760b5235c30b81927d33661e03a7
Ovidiu Cocolos <ioanovidiu.cocolos@gmail.com>
2021-07-05 11:58:54 +0300

    added Static assets, bootstrap & crispy_forms

--- pages/urls.py
+++ pages/urls.py
@@ -2,9 +2,10 @@
 from django.urls import path
 
 
-from .views import HomePageView
+from .views import HomePageView, AboutPageView
 
 
 urlpatterns = [
-    path('',HomePageView.as_view(), name='home')
+    path('',HomePageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about'),
 ]
