3f6dc28b7d7f8b15547c8e55c6c23a3c29a75f6b
Cocolos <ro00980@int.eberspaecher.com>
2021-07-09 14:39:20 +0300

    added Review option

--- books/models.py
+++ books/models.py
@@ -1,9 +1,10 @@
-from bookstore_project.settings import AUTH_PASSWORD_VALIDATORS
+# from bookstore_project.settings import AUTH_PASSWORD_VALIDATORS
 import uuid
+from django.contrib import auth
 
 from django.db import models
 from django.urls import reverse
-
+from django.contrib.auth import get_user_model
 
 
 class Book(models.Model):
@@ -21,3 +22,19 @@ class Book(models.Model):
 
     def get_absolute_url(self):
         return reverse('book_detail', args=[str(self.id)])
+
+
+class Review(models.Model):
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+    )
+    review = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.review
